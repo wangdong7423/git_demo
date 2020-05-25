24af09b7beead25bd86f0a257687845b858b785d
wangdong7423 <1814859090@qq.com>
2020-05-25 11:41:34 +0800

    项目第一次提交，项目架构

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("wangdong7423 第一次创建的新项目...");
+        System.out.println("wangdong7423 第一次创建的新项目...");
+        System.out.println("wangdong7423 第一次创建的新项目...");
+    }
+}
